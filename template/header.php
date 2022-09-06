88aff21c045518633027e19bf157f199f2a9c187
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2022-09-06 14:11:36 -0300

    add

--- template/header.php
+++ template/header.php
@@ -2,16 +2,15 @@
     <nav>
       <div class="navbar">
         <div class="container nav-container">
-        <div class="logo">
-            <h1>UEDA</h1>
-          </div>   
-        <input class="checkbox" type="checkbox" name="" id="" />
+            <input class="checkbox" type="checkbox" name="" id="" />
             <div class="hamburger-lines">
               <span class="line line1"></span>
               <span class="line line2"></span>
               <span class="line line3"></span>
             </div>  
-          
+          <div class="logo">
+            <h1>UEDA</h1>
+          </div>
           <div class="menu-items">
             <li><a href="#">Home</a></li>
             <li><a href="#">about</a></li>
