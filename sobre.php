987cb3e64e633610a768d5b4c71a7e5a8d213fc6
FELIPE GABRIEL SCHMITT <felipe.schmitt@alunos.sc.senac.br>
2022-09-14 14:09:46 -0300

    ajustes na pagina sobre

--- sobre.php
+++ sobre.php
@@ -8,45 +8,50 @@
 </head>
 <?php require("template/header1.php");?>
     <main>
+    <div class="margem-lados">
         <br><br>
         <center>
-            <h1 style=" font-weight: 100;" class="h1_título_topo">Projeto Integrador</h1>
+            <h1 style="font-text: 17px;" class="h1_título_topo">Projeto Integrador</h1>
         </center>
-        <br><br>
+        <br
         <div class="subtitulo-sobre">
-            <h1 style=" font-weight: 100;">O que é?</h1>
+            <p style="font-size: 17px;">O que é?</p>
         </div>
         <div class="sobre-txtprincipal">
-            <h1 style=" font-weight: 100;">O Projeto Integrador (PI) é uma Unidade Curricular, baseada na metodologia de ação-reflexão-ação, que se constitui na proposição de situações desafiadoras a serem cumpridas pelo aluno.</h1>
+            <p style="font-size: 17px; margin-left: 5%; margin-right: 5%;">O Projeto Integrador (PI) é uma Unidade Curricular, baseada na metodologia de ação-reflexão-ação, que se constitui na proposição de situações desafiadoras a serem cumpridas pelo aluno.</p>
         </div>
-        <br><br>
+        <br>
         <center>
         <div class="titulo-img-sobre">
-            <h1 style=" font-weight: 100;">Esta unidade curricular é obrigatória nos cursos de:</h1>
+            <p style="font-size: 17px;">Esta unidade curricular é obrigatória nos cursos de:</p>
         </div>    
         </center>
-        <br><br>
+        <br>
 
             <div class="container_images_sobre"> 
                 <div class="imgs-sobre">
-                    <img src="./images/sobre_img1.png" style="width:30%">
-                    <p style="text-align: center; font-size: 20px;">Aprendizagem Comercial</p>
+                    <img src="./images/sobre_img1.png" style="width:40%">
+                    <p style="text-align: center; font-size: 17px;">Aprendizagem Comercial</p>
                 </div>
                 <div class="imgs-sobre">
-                    <img src="./images/sobre_img2.png" style="width:30%">
-                    <p style="text-align: center; font-size: 20px;">Qualificação Profissional</p>
+                    <img src="./images/sobre_img2.png" style="width:40%">
+                    <p style="text-align: center; font-size: 17px;">Qualificação Profissional</p>
                 </div>
                 <div class="imgs-sobre">
-                    <img src="./images/sobre_img3.png" style="width:30%">
-                    <p style="text-align: center; font-size: 20px;">Cursos Técnicos</p>
+                    <img src="./images/sobre_img3.png" style="width:40%">
+                    <p style="text-align: center; font-size: 17px;">Cursos Técnicos</p>
                 </div>
             </div>
                 
 
-        <br><br><br><br><br>
+        <br><br>
         <div class="obj-sobre">
-            <img style="width: 350px;" src="./images/slogan_azul.png" alt="">
-            <h1 style=" font-weight: 100; font-size: 20px;">O planejamento e execução do Projeto Integrador propiciam a articulação das competências previstas no perfil profissional de conclusão do curso, pois apresenta ao aluno situações que estimulam o seu desenvolvimento profissional ao ter que decidir, opinar e debater com o grupo a resolução de problemas a partir do tema gerador, portanto, o aluno poderá demonstrar sua atuação profissional pautada pelas marcas formativas do Senac, uma vez que permite o trabalho em equipe e o exercício da ética, da responsabilidade social e da atitude empreendedora.</h1>
+            <div class="logo-sobre">
+                <img style="width: 350px;" src="./images/slogan_azul.png" alt="">
+            </div>
+            <div class="obj-sobre-txt">
+                <h1 style=" font-weight: 100; font-size: 17px;">O planejamento e execução do Projeto Integrador propiciam a articulação das competências previstas no perfil profissional de conclusão do curso, pois apresenta ao aluno situações que estimulam o seu desenvolvimento profissional ao ter que decidir, opinar e debater com o grupo a resolução de problemas a partir do tema gerador, portanto, o aluno poderá demonstrar sua atuação profissional pautada pelas marcas formativas do Senac, uma vez que permite o trabalho em equipe e o exercício da ética, da responsabilidade social e da atitude empreendedora.</h1>
+            </div> 
         </div>
         <div class="margem-lados">
         </div>
@@ -55,5 +60,6 @@
                 <h1 style="font-weight: 100; font-size: 1.3rem;">Adriano Ramos | Felipe Gabriel Schmitt | Gabrieli Eduarda Lembeck | Lucas Inacio Diomario Coelho | Nina Carolina Lima Barater</h1>
             </div>
         </div>
+</div>
     </main>
 <?php require("template/footer1.php");?>
