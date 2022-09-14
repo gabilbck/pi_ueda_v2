d50428e19295d468e36e79f28bac0604c7cd0b36
ninaclb <ninacarolinabarater@gmail.com>
2022-09-14 14:08:21 -0300

    add

--- adm/listas/lista_usu.php
+++ adm/listas/lista_usu.php
@@ -8,6 +8,38 @@
 </head>
 <?php require("../../template/header3.php");?>
     <main>
+        <style>
+        tr:nth-child(even) {
+            background:lightgray;
+        }
+
+        table{
+            background: #f2f2f2;
+        }
+        </style>
+        <table>
+            <tr>
+                <th>abc</th>
+                <th>abc</th>
+                <th>abc</th>
+            </tr>
+            <tr>
+                <td>abc</td>
+                <td>abc</td>
+                <td>abc</td>
+            </tr>
+            <tr>
+                <td>abc</td>
+                <td>abc</td>
+                <td>abc</td>
+            </tr>
+            <tr>
+                <td>abc</td>
+                <td>abc</td>
+                <td>abc</td>
+            </tr>
+
+        </table>
 
     </main>
 <?php require("../../template/footer3.php");?>
