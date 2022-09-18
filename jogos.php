d9f6a35cb72a105d585b91a8409f11cd6d8cd0b0
Felipe-G-Schmitt <felipegabrielsc2006@gmail.com>
2022-09-18 02:33:46 -0300

    inicio página principal de jogos

--- jogos.php
+++ jogos.php
@@ -24,11 +24,43 @@
                 </div>
                 <div class="principais-text-jogos2">
                     <h1 class="descricao_titulo">Descrição</h1>
-                    <h1 class="descricao">Um simples jogo onde você controla um <br>dinossauro, onde seu objetivo é alcançar <br>a maior pontuação sem encostar nos obstáculos.</h1>
-                    <h1 class="num_jogado">Categoria: Simples</h1>
+                    <h1 class="descricao">Um simples jogo onde você controla uma <br>gota de água e desvia de obstáculos.</h1>
+                    <h1 class="obj-jogo">Objetivo</h1>
+                    <h1 class="obj-jogo2">O objetivo do jogo é alcançar a maior pontuação evitando os obstáculos, que são os lixos no oceano.
                 </div> 
-            </div> <br><br><br><br><br><br>
+            </div> <br><br><br><br><br><br><br><br>
+            <?php
             
+            $sql = $pdo->prepare("SELECT * FROM jogos");
+            if ($sql->execute()){
+                $info = $sql->fetchAll(PDO::FETCH_ASSOC);}
+
+                foreach($info as $key => $values){
+                    echo "<div class='jogos'>";
+                    $image_jogo = $values['image_jogo'];
+                    echo '<img style="width: 350px;" src="data:image/jpg;charset=utf8;base64,'.base64_encode($image_jogo).'"/>';
+                    echo $values['nome_jogo'];
+                    echo $values['desc_jogo'];
+                    echo $values['link_jogo'];
+                    echo "</div>";
+                }
+
+            /*echo "<div class='section-jogos'>";
+                foreach($info as $key => $values){
+                    echo "<div class='jogos'>";
+                    $image_jogo = $values['image_jogo'];
+                    echo '<img style="width: 350px;" src="data:image/jpg;charset=utf8;base64,'.base64_encode($image_jogo).'"/>';
+                    echo $values['nome_jogo'];
+                    echo $values['desc_jogo'];
+                    echo $values['link_jogo'];
+                    echo "</div>";
+                }
+            echo "</div>";*/
+            
+
+            $image_jogo = $values['image_jogo'];
+            base64_encode($image_jogo);
+            ?> 
         </div>
         <br><br>
     </main>
