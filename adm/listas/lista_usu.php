19d4797ab74df13fcc204cad7dc0526e23535758
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2022-09-20 11:36:20 -0300

    add

--- adm/listas/lista_usu.php
+++ adm/listas/lista_usu.php
@@ -8,7 +8,46 @@
 </head>
 <?php require("../../template/header3.php");?>
     <main>
-        <table class="listagens-table">
+        <div class="margem-lados">
+            <?php
+                $sql = $pdo->prepare('SELECT * FROM usuario');
+                if ($sql->execute()){
+                    $info = $sql->fetchAll(PDO::FETCH_ASSOC);
+            
+                    echo "<center>";
+                    echo "<h1>Listagem de Usuários</h1>";
+                    echo "<table class='listagens-table'";
+                    echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>Nome</th>";
+                    echo "<th>E-mail</th>";
+                    echo "<th>Senha</th>";
+                    echo "<th>Nome Completo</th>";
+                    echo "<th>Administrador</th>";
+                    echo "<th>Alterar</th>";
+                    echo "<th>Excluir</th>";
+                    echo "</tr>";
+            
+                    foreach($info as $key => $value){
+                        echo "<tr>";
+                        echo "<td>".$value['id_usu']."</td>";
+                        echo "<td>".$value['nome_usu']."</td>";
+                        echo "<td>".$value['email_usu']."</td>";
+                        echo "<td>".$value['senha_usu']."</td>";
+                        echo "<td>".$value['nome_real_usu']."</td>";
+                        echo "<td>".$value['adm']."</td>";
+                        echo "<td><center><a class='alt' href='../del/del_usu.php?id_usu=".$value['id_usu']."'>(+)</a></center></td>";
+                        echo "<td><center><a class='del' href='../alt/alt_usu.php?id_usu=".$value['id_usu']."'>(-)</a></center></td>";
+                        echo "</tr>";
+                    }
+            
+                    echo "</table>";
+                    echo "<br><button><a href='../../login/cadastro.php'>Cadastrar novo usuário</a></button>";
+                    echo "</center>";
+                }
+            ?>
+        </div>
+        <!-- <table class="listagens-table">
             <tr>
                 <th>abc</th>
                 <th>abc</th>
@@ -29,8 +68,6 @@
                 <td>abc</td>
                 <td>abc</td>
             </tr>
-
-        </table>
-
+        </table> -->
     </main>
 <?php require("../../template/footer3.php");?>
