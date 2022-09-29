d1d5e333f6f43b432dd29b2813cd04af02cbf62c
nina carolina <ninacarolinabarater@gmail.com>
2022-09-29 16:53:24 -0300

    add

--- bug.php
+++ bug.php
@@ -5,8 +5,6 @@
     
     $email_bug = $desc_bug = $cod_bug =  "";
     $email_bugErr = $desc_bugErr = $msgErr = "";
-    $id_usu = $_SESSION['id_usu'];
-
 
     if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])){
         if (empty($_POST['email_bug'])){
@@ -24,10 +22,10 @@
         if ($email_bug && $desc_bug) {
                     //Inserir no banco de dados
             $sql = $pdo->prepare("INSERT INTO bug (cod_bug, email_bug, desc_bug)
-                                VALUES (null, ?, ?, ?)");
+                                VALUES (null, ?, ?)");
             if ($sql->execute(array($email_bug, $desc_bug))){
                 $msgErr = "Erro reportado com sucesso!";  
-                header("location: bug.php");
+                header('location:index.php');
             } else {
                 $msgErr = "Erro não reportado!";
             }
