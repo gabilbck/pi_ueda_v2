89ca3b777233e793440f1f48ab02d3f0d384fde1
Felipe-G-Schmitt <felipegabrielsc2006@gmail.com>
2022-10-02 14:35:52 -0300

    adicionado jogo

--- jogos.php
+++ jogos.php
@@ -16,25 +16,28 @@
         <div class="margem-lados">
             <div class="principal-jogos">
                 <div class="principais-text-jogos">
-                    <h1 class="titulo_principal">Jogo do dino</h1>
+                    <h1 class="titulo_principal">Jogo da gota</h1>
                     <h1 class="subtitulo_principal">Desenvolvido por: Equipe UEDA</h1>
                 </div>
-                <div class="jogo_ueda">
-                    <img style="width:90%;" src="./images/temp_image_jogos.png" alt="">    
-                </div>
+                <a href="./jogo/jogo-ueda/index.html">
+                    <div class="jogo_ueda">
+                        <img style="width:100%;" src="./images/banner-jogo.png" alt="">    
+                    </div>    
+                </a>
                 <div class="principais-text-jogos2">
                     <h1 class="descricao_titulo">Descrição</h1>
                     <h1 class="descricao">Um simples jogo onde você controla uma <br>gota de água e desvia de obstáculos.</h1>
                     <h1 class="obj-jogo">Objetivo</h1>
                     <h1 class="obj-jogo2">O objetivo do jogo é alcançar a maior pontuação evitando os obstáculos, que são os lixos no oceano.
                 </div> 
-            </div> <br><br><br><br><br><br><br><br>
+            </div>
             <?php
             
             $sql = $pdo->prepare("SELECT * FROM jogos");
             if ($sql->execute()){
                 $info = $sql->fetchAll(PDO::FETCH_ASSOC);}
 
+                echo "<div class='jogos-agua'>";
                 foreach($info as $key => $values){
                         $image_jogo = $values['image_jogo']; 
                         echo '<img style="max-width:400px; max-height:200px; width: auto; height: auto;" src="data:image/jpg;charset=utf8;base64,'.base64_encode($image_jogo).'"/>'; 
@@ -42,6 +45,7 @@
                         echo $values['desc_jogo'];
                         echo $values['link_jogo']; 
                     }
+                echo "</div>";
                 
                 
                 /*foreach($info as $key => $values){
