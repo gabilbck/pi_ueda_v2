d58db38493bf8a973fb5cc0c3909b34d56265b56
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2022-10-06 08:32:20 -0300

    paginas de mensagem de sucesso

---
+++ paginas/sus_cad.php
@@ -0,0 +1,18 @@
+<?php require("../template/header.php");?>
+<head>
+    <title>CADASTRADO COM SUCESSO! | UEDA</title>
+</head>
+    <main>
+        <div class="margem-lados">
+            <center>
+                <br><br>
+                <h1>ITEM CADASTRADO COM SUCESSO!</h1>
+                <br>
+                <h2>Voltar à página principal:</h2>
+                <br>
+                <a href="home.php"><button>PÁGINA PRINCIPAL</button></a>
+                <br><br>
+            </center>
+        </div>
+    </main>
+<?php require("../template/footer.php");?>
---
+++ paginas/sus_cad_usu.php
@@ -0,0 +1,18 @@
+<?php require("../template/header.php");?>
+<head>
+    <title>CADASTRADO COM SUCESSO! | UEDA</title>
+</head>
+    <main>
+        <div class="margem-lados">
+            <center>
+                <br><br>
+                <h1>VOCÊ FOI CADASTRADO COM SUCESSO!</h1>
+                <br>
+                <h2>Vá à página de login clicando no botão logo abaixo, preencha às informações solicitadas e desfrute dos recursos disponíveis em nosso site!</h2>
+                <br>
+                <a href="login_usu.php"><button>FAÇA LOGIN</button></a>
+                <br><br>
+            </center>
+        </div>
+    </main>
+<?php require("../template/footer.php");?>
