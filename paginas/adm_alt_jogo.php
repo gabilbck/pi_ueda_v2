2cfee538ba9d6fce6635cb0a2f28c99ca2bd0f7d
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2022-10-13 09:17:41 -0300

    arrumei cadastro. lista e alt do jogo

--- paginas/adm_alt_jogo.php
+++ paginas/adm_alt_jogo.php
@@ -15,7 +15,7 @@
         if ($sql->execute(array($codigo))){
             $info = $sql->fetchAll(PDO::FETCH_ASSOC);
             foreach($info as $key => $value){
-                $cod_Jogo = $value['cod_jogo'];
+                $cod_jogo = $value['cod_jogo'];
                 $nome_jogo = $value['nome_jogo'];
                 $desc_jogo = $value['desc_jogo'];
                 $image_jogo = $value['image_jogo'];
@@ -25,48 +25,48 @@
     }
 
     if (isset($_POST["submit"])){
+
         if (!empty($_FILES["image"]["name"])){
             //Pegar informações
             $fileName = basename($_FILES["image"]["name"]);
             $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
             //Permitir somente alguns formatos
             $allowTypes = array('jpg', 'png', 'jpeg', 'gif');
-
             if (in_array($fileType, $allowTypes)){
                 $image = $_FILES['image']['tmp_name'];
                 $imgContent = file_get_contents($image);
+            } else {
+                $msgErr = "Desculpe, mas somente arquivos JPG, JPEG, PNG e GIF são permitidos";
+            }
+        }
 
-                if (isset($_POST['nome_jogo'])){
-                    $nome_jogo = $_POST['nome_jogo'];
-                } else {
-                    $nome_jogo = "";
-                }
-                if (isset($_POST['desc_jogo'])){
-                    $desc_jogo = $_POST['desc_jogo'];
-                } else {
-                    $desc_jogo = "";
-                }
-                if (isset($_POST['link_jogo'])){
-                    $link_jogo = $_POST['link_jogo'];
-                } else {
-                    $link_jogo = "";
-                }
+        if (!empty($_POST['nome_jogo'])){
+            $nome_jogo = $_POST['nome_jogo'];
+        } else {
+            $nome_jogoErr = "Erro no nome do jogo";
+        }
+        if (!empty($_POST['desc_jogo'])){
+            $desc_jogo = $_POST['desc_jogo'];
+        } else {
+            $desc_jogoErr = "Erro na descrição do jogo";
+        }
+        if (!empty($_POST['link_jogo'])){
+            $link_jogo = $_POST['link_jogo'];
+        } else {
+            $link_jogoErr = "Erro no link do jogo";
+        }
 
-                $sql = $pdo->prepare("UPDATE jogos SET nome_jogo=?, desc_jogo=?, link_jogo=?, image_jogo=? WHERE cod_jogo=?");
-                if ($sql->execute(array($nome_jogo, $desc_jogo, $link_jogo, base64_encode($imgContent)))){
-                   $msgErr = "Dados alterados com sucesso!";
-                    header('location: adm_lista_jogo.php');
-                } else{
-                    $msgErr = "Dados não alterados.";
-                    
-                }
+        $sql = $pdo->prepare("UPDATE jogos SET nome_jogo=?, desc_jogo=?, link_jogo=?, image_jogo=? WHERE cod_jogo=?");
+        if ($sql->execute(array($nome_jogo, $desc_jogo, $link_jogo, base64_encode($imgContent), $cod_jogo))){
+            $msgErr = "Dados alterados com sucesso!";
+            header('location: adm_lista_jogo.php');
+        } else{
+            $msgErr = "Dados não alterados.";
+            
+        }
 
-           } else {
-               $msgErr = "Desculpe, mas somente arquivos JPG, JPEG, PNG e GIF são permitidos";
-           }
-       } else {
-           $msgErr = "Informações incorretas";
-       }
+    } else {
+        $msgErr = "Desculpe, mas somente arquivos JPG, JPEG, PNG e GIF são permitidos";
     }
 ?>
 <head>
@@ -78,24 +78,26 @@
                 <br><br>
                 <h1>ALTERAR</h1>
                 <br>
-                <form action="" method="post">
+                <form action="" method="post"  enctype="multipart/form-data">
                     <input type="text" name="cod_jogo" value="<?php echo $cod_jogo?>" readonly>
                     <span class="n-obrigatorio">*</span>
                     <br><br>
                     <input name="nome_jogo" value="<?php echo $nome_jogo?>" type="text" placeholder="Nome do jogo">
-                    <span class="obrigatorio">*</span>
+                    <span class="obrigatorio">* <?php echo '<br>'.$nome_jogoErr ?></span>
                     <br><br>
                     <input name="desc_jogo" value="<?php echo $desc_jogo?>" type="text" placeholder="Descrição do jogo">
-                    <span class="obrigatorio">*</span>
+                    <span class="obrigatorio">* <?php echo '<br>'.$desc_jogoErr ?></span>
                     <br><br>
                     <input name="link_jogo" value="<?php echo $link_jogo?>" type="text" placeholder="Link do jogo">
-                    <span class="obrigatorio">*</span>
+                    <span class="obrigatorio">* <?php echo '<br>'.$link_jogoErr ?></span>
                     <br><br>
-                    <label for="imagem">Imagem: </label>
-                    <input type="file" name="imagem" value="<?php echo $imagem?>"/><br><br>
+                    <div class="escolha-imagem">
+                        <label for="image">Selecione uma imagem (Opcional)</label>
+                        <input type="file" id='image' name="image"/><br><br>
+                    </div>
                     <div class="botoes-alt">
-                        <button type="submit" name="cadastro">ALTERAR</button>
-                        <button><a class="link-branco" href="adm_lista_eti.php">VOLTAR</a></button>
+                        <button type="submit" name="submit">ALTERAR</button>
+                        <button><a class="link-branco" href="adm_lista_jogo.php">VOLTAR</a></button>
                     </div>
                 </form>
                 <br><br>
@@ -103,4 +105,3 @@
         </div>
     </main>
 <?php require("../template/footer.php");?>
-
