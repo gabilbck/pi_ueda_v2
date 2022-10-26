80f780195895b5a72c723d9b33ce07b4f334361b
Felipe-G-Schmitt <felipegabrielsc2006@gmail.com>
2022-10-26 09:22:01 -0300

    jogos

--- paginas/menu_jogos.php
+++ paginas/menu_jogos.php
@@ -12,7 +12,7 @@
             <div class="principal-jogos">
                 <div class="principais-text-jogos">
                     <h1 class="titulo_principal">Jogo da gotinha</h1>
-                    <!--<h1 class="subtitulo_principal">Desenvolvido por: Equipe UEDA</h1>-->
+                    <h1 class="subtitulo_principal">Desenvolvido por: Equipe UEDA</h1>
                 </div>
                 <a target='_blank' href="../jogo/jogo-ueda/index.html">
                     <div class="jogo_ueda">
@@ -22,8 +22,8 @@
                 <div class="principais-text-jogos2">
                     <h1 class="descricao_titulo">Descrição</h1>
                     <h1 class="descricao">Um simples jogo onde você controla uma <br>gota de água e desvia de obstáculos.</h1>
-                    <!--<h1 class="obj-jogo">Objetivo</h1>-->
-                    <!--<h1 class="obj-jogo2">O objetivo do jogo é alcançar a maior pontuação evitando os obstáculos, que são os lixos no oceano.</h1>-->
+                    <h1 class="obj-jogo">Objetivo</h1>
+                    <h1 class="obj-jogo2">O objetivo do jogo é alcançar a maior pontuação evitando os obstáculos, que são os lixos no oceano.</h1>
                 </div>
             </div>
             </div>
@@ -45,12 +45,16 @@
                                             echo '<img style="width:400px; max-height:200px; width: auto; height: auto;" src="data:image/jpg;charset=utf8;base64,'.$image_jogo.'"/>';
                                         echo "</div>";
                                         echo "<div class='jogo-info'>";
-                                            echo "<div class='nome-jogos'>";
-                                                echo $values['nome_jogo']."<br>";
+                                            echo "<div class='warpper-nome'>";
+                                                echo "<div class='nome-jogos'>";
+                                                    echo $values['nome_jogo']."<br>";
+                                                echo "</div>";
                                             echo "</div>";
-                                            echo "<div class='desc-info'>";
-                                                echo $values['desc_jogo']."<br>";
-                                            echo "</div>";
+                                                echo "<div class='warpper-desc'>";
+                                                    echo "<div class='desc-info'>";
+                                                        echo $values['desc_jogo']."<br>";
+                                                    echo "</div>";
+                                                echo "</div>";
                                             echo "<br>";
                                                 echo "<div class='warpper'>";
                                                     echo "<div class='link-info'>";
