8ab2175f5e6feabab6c21cfcfd01ea3eb0a35993
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2022-11-09 13:56:37 -0300

    arrumei login

--- paginas/login_usu.php
+++ paginas/login_usu.php
@@ -6,37 +6,8 @@
     
     $email_usu = $senha_usu = $msgErr = "";
     $email_usuErr = $senha_usuErr = "";
-
-    if ($_SERVER['REQUEST_METHOD'] == "POST"){
-        if (empty($_POST['email_usu'])){
-            $email_usuErr = "Email está incorreto.";
-        } else {
-            $email_usu = test_input($_POST["email_usu"]);
-        }
-        if (empty($_POST['senha_usu'])){
-            $senha_usuErr = "Senha está incorreta.!";
-        } else {
-            $senha_usu = test_input($_POST["senha_usu"]);
-        }
-
-        if ($email_usu && $senha_usu){
-            //código para consultar os dados no banco de dados
-            $sql = $pdo->prepare("SELECT * FROM usuario WHERE email_usu = ? AND senha_usu = ?");
-            if ($sql->execute(array($email_usu, MD5($senha_usu)))){
-                $info = $sql->fetchAll(PDO::FETCH_ASSOC);
-                
-                if (count($info) > 0){
-                    foreach($info as $key => $values){
-                        $_SESSION['id_usu'] = $values['id_usu'];
-                        $_SESSION['nome_usu'] = $values['nome_usu'];
-                        $_SESSION['adm'] = $values['adm'];
-                    }
-                    header('location:sus_log_usu.php');
-                } else {
-                    $msgErr = '* Credenciais incorretas ou não informadas.';
-                }
-            }
-        }
+    if(isset($_GET['msgErr'])){
+        $msgErr = $_GET['msgErr'];
     }
 ?>
 <head>
@@ -48,7 +19,7 @@
                 <br><br>
                 <h1>LOGIN</h1>
                 <br>
-                <form action="" method="post">
+                <form action="login_usu_controler.php" method="post">
                     <input name="email_usu" value="<?php echo $email_usu?>" type="email" placeholder="E-mail">
                     <br><br>
                     <input name="senha_usu" value="<?php echo $senha_usu?>" type="password" placeholder="Senha">
---
+++ paginas/login_usu_controler.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    include_once "../include/MySql.php";
+    include_once "../include/functions.php";
+?>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+    if (empty($_POST['email_usu'])){
+        $email_usuErr = "Email está incorreto.";
+    } else {
+        $email_usu = test_input($_POST["email_usu"]);
+    }
+    if (empty($_POST['senha_usu'])){
+        $senha_usuErr = "Senha está incorreta.!";
+    } else {
+        $senha_usu = test_input($_POST["senha_usu"]);
+    }
+
+    if ($email_usu && $senha_usu){
+        //código para consultar os dados no banco de dados
+        $sql = $pdo->prepare("SELECT * FROM usuario WHERE email_usu = ? AND senha_usu = ?");
+        if ($sql->execute(array($email_usu, MD5($senha_usu)))){
+            $info = $sql->fetchAll(PDO::FETCH_ASSOC);
+            
+            if (count($info) > 0){
+                foreach($info as $key => $values){
+                    $_SESSION['id_usu'] = $values['id_usu'];
+                    $_SESSION['nome_usu'] = $values['nome_usu'];
+                    $_SESSION['adm'] = $values['adm'];
+                }
+                header('location:sus_log_usu.php');
+            } else {
+                $msgErr = '* Credenciais incorretas ou não informadas.';
+                header('location:login_usu.php?msgErr='.$msgErr);
+            }
+        }
+    }
+}
+?>
